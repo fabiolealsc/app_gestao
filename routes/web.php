504295e92d55ebead06fe74b34e28785866dad21
Fabio Leal Schmitz <86978853+fabiolealsc@users.noreply.github.com>
2024-05-15 09:35:29 -0300

    Testes

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,37 @@ use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\ContatoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/sobrenos', [SobreNosController::class, 'sobrenos'])->name('site.sobrenos');
 
-Route::get('/contato', [ContatoController::class, 'contato']);
+Route::get('/login', function () {
+    return 'Login';
+})->name('site.login');
 
-Route::get('/sobrenos', [SobreNosController::class, 'sobrenos']);
+Route::prefix('/app',)->group(function () {
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    })->name('app.clientes');
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    })->name('app.fornecedores');
+    Route::get('/produtos', function () {
+        return 'Produtos';
+    })->name('app.produtos');
+});
+Route::fallback(function () {
+    return view('site.principal');
+});
+
+
+
 
 /* TIPOS DE VERBOS
-Route::get($uri, $callback);
-Route::post($uri, $callback);
-Route::put($uri, $callback);
-Route::patch($uri, $callback);
-Route::delete($uri, $callback);
-Route::options($uri, $callback);
+    Route::get($uri, $callback);
+    Route::post($uri, $callback);
+    Route::put($uri, $callback);
+    Route::patch($uri, $callback);
+    Route::delete($uri, $callback);
+    Route::options($uri, $callback);
 */
