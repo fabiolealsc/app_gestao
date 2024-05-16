30951c8c9a8f30704d69989020e68efd00910ecd
Fabio Leal Schmitz <86978853+fabiolealsc@users.noreply.github.com>
2024-05-16 12:51:06 -0300

    Factory

--- database/seeders/SiteContatoSeeder.php
+++ database/seeders/SiteContatoSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\SiteContato;
+use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,14 +14,15 @@ class SiteContatoSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i = 0; $i < 100; $i++) {
+        /*for ($i = 0; $i < 100; $i++) {
             $contato = new SiteContato();
-            $contato->nome = 'contato';
+            $contato->nome = 'contato' . $i;
             $contato->telefone = '"(' . rand(0, 9) . rand(0, 9) . ')' . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . '"';
             $contato->email = 'contato' . $i . '@contato.com.br';
             $contato->motivo_contato =  rand(1, 3);
             $contato->mensagem = 'Mensagem ' . $i . ' enviada automaticamente';
             $contato->save();
-        }
+        }*/
+        SiteContato::factory()->count(100)->create();
     }
 }
