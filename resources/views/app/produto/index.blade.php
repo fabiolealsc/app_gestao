e3f3f0188203700ef42f5d1dd4864e5da97b81cf
Fabio Leal Schmitz <86978853+fabiolealsc@users.noreply.github.com>
2024-06-10 17:47:56 -0300

    Foi

--- resources/views/app/produto/index.blade.php
+++ resources/views/app/produto/index.blade.php
@@ -15,7 +15,6 @@
         </div>
         <div class="informacao-pagina">
             <div style="width: 90%; margin-left: auto; margin-right: auto;">
-                {{ $produtos->toJson() }}
                 <table border="1" width='100%'>
                     <thead>
                         <tr>
@@ -58,7 +57,6 @@
                         @endforeach
                     </tbody>
                 </table>
-                {{ $produtos->toJson() }}
                 {{ $produtos->appends($request)->links() }}
                 
             </div>
